e57346e1ac68baaa189df682792432b50782cda4
Julien REMY <Julien REMY>
2016-12-03 18:13:18 +0100

    recherche reference + achat client supermarche

--- src/main/java/fr/miage/agents/api/message/recherche/ResultatRecherche.java
+++ src/main/java/fr/miage/agents/api/message/recherche/ResultatRecherche.java
@@ -10,6 +10,15 @@ import java.util.List;
  * Created by nitix on 14/11/16.
  */
 public class ResultatRecherche extends Message{
+	
+	/**
+	 * ProduitUnique si la recherche est effectué sur une reférence
+	 * le supermarché doit retourner le prix du produit
+	 */
+	
+	public boolean isProduitUnique;
+	
+	public float prixProduitUnique;
 
     public List<Produit> produitList;
 
